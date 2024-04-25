503984f3b6489f6f87df21701ffe7d109f155907
Feiron <53085628+fr1g@users.noreply.github.com>
2024-04-25 19:46:58 +0800

    为什么得写两次才对

--- src/src/App.vue
+++ src/src/App.vue
@@ -18,12 +18,21 @@ export default{
     },
     methods: {
         GetColorName(){
+            localStorage.setItem('accent', window.getComputedStyle(document.body).getPropertyValue('background-color'));
             return this.GlobalColorName;
         },
         SetColorName(name){
             this.GlobalColorName = (name ?? 'emerald').toLowerCase();
             this.$forceUpdate();
             localStorage.setItem('color', this.GlobalColorName);
+            localStorage.setItem('accent', window.getComputedStyle(document.body).getPropertyValue('background-color'));
+            // document.body.classList.remove
+            let _tmp = '';
+            localStorage.setItem('accent', window.getComputedStyle(document.body).getPropertyValue('background-color'));
+            for(let cc of document.body.classList){
+                if(cc.includes('bg-')) _tmp = cc;
+            }
+            document.body.classList.replace(_tmp, `bg-${this.GlobalColorName}-100`);
         }
     },
     provide(){
