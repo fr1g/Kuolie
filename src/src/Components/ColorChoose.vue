99418864a2dd9a10f7e4431f502045bcc1ec55a1
Feiron <53085628+fr1g@users.noreply.github.com>
2024-05-02 02:53:14 +0800

    Update ColorChoose.vue

--- src/src/Components/ColorChoose.vue
+++ src/src/Components/ColorChoose.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="grid grid-cols-1 gap-1.5 ">
-        <div v-for="color of this.colors" :key="color" :title="color" @click="C(color)" :id="`COLORBTN:${color.toLowerCase()}`"
+        <div v-for="color of this.colors" :key="color" :title="color.replaceAll(/-\d{3}/g, '')" @click="C(color)" :id="`COLORBTN:${color.toLowerCase()}`"
          :class="`w-6 h-6 transition hover:scale-110 scale-105 active:scale-100 box-border overflow-hidden
          rounded-lg opacity-70 hover:opacity-90 active:opacity-100 shadow border border-opacity-50 relative
          bg-${processMixColor(color.toLowerCase(), 0)}-500 `">
