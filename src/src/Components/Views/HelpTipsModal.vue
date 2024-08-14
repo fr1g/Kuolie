6e0b1c5bf3db64f3e90ae2a21fb809f50be01032
Feiron <53085628+fr1g@users.noreply.github.com>
2024-08-14 15:17:01 +0800

    Update HelpTipsModal.vue

--- src/src/Components/Views/HelpTipsModal.vue
+++ src/src/Components/Views/HelpTipsModal.vue
@@ -6,6 +6,7 @@
             
             输入框具有简单的输入补全, 可用于补全方括号和HTML标签以及图标标记 <code>%=&lt;PUA&gt;%</code>. 将会在按下等于号的时候触发补全. <br>
             按下回车将自动在html标签之外的地方插入换行符. <br>
+            如果只希望编辑器中换行, 可以按住shift然后再按回车. <br>
             在开标签末尾或标签名后有一个空格的情况下输入等号将会补全一对引号并将输入光标转移到引号中.
             
         </p>
@@ -16,6 +17,7 @@
             如需要插入Segoe Fluent Icon到文本块里, 需要使用<code>%=&lt;PUA&gt;%</code>插入. <br>
             比如, %=e711%等价于&#xe711; / \ue711. <br>
             查询SFI PUA代码: <a class="a modala " style="pointer-events: all;" href="https://learn.microsoft.com/zh-cn/windows/apps/design/style/segoe-fluent-icons-font#pua-e700-e900" target="_blank">查看PUA映射表</a><br>
+            使用%=n% 来插入多个空格, 将n替换为你需要的数量.<br>
             在按下回车时, 将自动插入一个<b>HTML换行符</b>到光标处.
         </p>
         <h2 class="modalx text-lg font-semibold mt-1">其他信息</h2>
