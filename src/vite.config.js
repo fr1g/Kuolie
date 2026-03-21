86f716c567cd7c9d8ae6760af3702466d7312b2c
fr1g <cheunghast@outlook.com>
2026-03-21 15:53:15 +0300

    local dir using exfat, unable to link

--- src/vite.config.js
+++ src/vite.config.js
@@ -4,26 +4,28 @@ import { defineConfig } from 'vite'
 import legacy from '@vitejs/plugin-legacy'
 import vue2 from '@vitejs/plugin-vue2'
 import { createVuePlugin } from '@vitejs/plugin-vue2' // ?
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {  
+  resolve: {
     alias: {
       vue: 'vue/dist/vue.esm.js',
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
   plugins: [
-    vue2({ 
+    vue2({
       runtimeCompiler: true,
     }),
     legacy({
       targets: ['ie >= 11'],
       additionalLegacyPolyfills: ['regenerator-runtime/runtime']
     }),
+    tailwindcss()
     // createVuePlugin.vueTemplateOptions({
     //   runtimeCompiler: true,
     // })
   ],
-  base: './', 
+  base: './',
 })
